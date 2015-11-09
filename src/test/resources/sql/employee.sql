9c35ec358a239ffaa8dd382f532284fda85a7734
mathyChris <maphys1905@gmail.com>
2015-11-09 14:50:32 +0900

    first commit

---
+++ src/test/resources/sql/employee.sql
@@ -0,0 +1,6 @@
+
+
+
+select * from dept ;
+
+select *from emp ;
---
+++ src/test/resources/sql/world.sql
@@ -0,0 +1,12 @@
+
+
+
+select *from dept ;
+
+select *from city ;
+
+select count(*) from city ;
+
+select *from country; 
+
+select count(*) from country; 
